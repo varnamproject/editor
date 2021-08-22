e60c6402884b1d5844132e0c0467237db7c6972d
Subin Siby <mail@subinsb.com>
2021-08-22 23:38:46 +0530

    Update contributors link

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -8,7 +8,7 @@
     >
       <v-card-text>
         Need Help ? Feedback ? <a href="https://indicproject.org/" target="_blank">Contact Us</a><br/><br/>
-        <a href="https://varnamproject.com/" target="_blank">Varnam</a> is a Free Software under&nbsp;<a href="https://indicproject.org/" target="_blank">Indic Project</a>. Copyright 2020, <a href="https://github.com/athul/varnam-desktop/graphs/contributors" target="_blank">Varnam Contributors</a>.
+        <a href="https://varnamproject.com/" target="_blank">Varnam</a> is a Free Software under&nbsp;<a href="https://indicproject.org/" target="_blank">Indic Project</a>. Copyright 2020, <a href="https://github.com/varnamproject/editor/graphs/contributors" target="_blank">Varnam Contributors</a>.
       </v-card-text>
     </v-card>
   </v-footer>
